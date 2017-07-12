fa7ff05c2549e8b935dca10866fb1f45f267ea31
boa <boa@promobit.ru>
2017-07-12 17:51:14 +0600

    added startListen method to class TcpServer

--- network/src/client_socket.cpp
+++ network/src/client_socket.cpp
@@ -4,5 +4,18 @@
 namespace calc_server {
     namespace network {
 
+        ClientSocket::ClientSocket(QObject *parent) : QTcpSocket(parent) {
+            connect(this, &ClientSocket::readyRead, this, &ClientSocket::readClient);
+            connect(this, &ClientSocket::disconnected, this, &ClientSocket::deleteLater);
+        }
+
+        ClientSocket::~ClientSocket() {
+            auto str = "Client\t" + sockeName_ + "\thas disconnected";
+            emit disconnects(str);
+        }
+
+        void ClientSocket::readClient() {
+        }
+
     }
 }
